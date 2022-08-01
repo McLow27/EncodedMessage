f5cd50fadf20caff25a87be304bb3b2be39f7d94
McLow27 <95956527+McLow27@users.noreply.github.com>
2022-08-01 15:28:57 -0400

    Add files via upload

---
+++ EncodedMessage/Encoded.java
@@ -0,0 +1,21 @@
+public class Encoded {
+    public static void main(String[] args) {
+        String encode = "";
+        char[][] decode = {
+                { 'e', 'e', 'd', 'A', 'R' },
+                { 'B', 't', 'V', 'r', 'o' },
+                { 'l', 's', 'i', 'e', 's' },
+                { 'u', 'A', 'o', 'R', 'e' },
+                { 'e', 'r', 'l', 'e', 's' }
+        };
+
+        for (int i = decode.length - 1; i >= 0; i--) {
+            for (int j = 0; j < decode.length; j++) {
+                encode += decode[j][i];
+
+            }
+        }
+        System.out.println(encode);
+
+    }
+}
